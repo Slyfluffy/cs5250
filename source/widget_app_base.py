e695c68613cb9588f5c21b6d47aa9b26160e7bb0
Journey Curtis <programming.jlcsly98@icloud.com>
2024-10-17 02:17:10 +0000

    Add argument options to consumer

--- source/widget_app_base.py
+++ source/widget_app_base.py
@@ -2,15 +2,68 @@
 needed to work with AWS.
 '''
 
+from argparse import ArgumentParser
+from logging import Logger
+
 class WidgetAppBase():
     '''Base class for all Widget Apps. Contains the needed data to work with AWS, as well as shared
     app defaults.
     '''
-    # The following are the defaults (note implementations may
-    # be different)
-    profile:str = 'default'
-    region:str = 'us-east-1'
-    max_runtime:int = 0
-    request_bucket:str = None
-    use_owner_in_prefix: str = 'widgets/'
-    request_queue_url:str = None
+    def __init__(self) -> None:
+        self.logger = Logger('WidgetAppBase')
+
+    def _verify_base_arguments(self, args: object) -> bool:
+        '''Verifies that the base needed arguments are met. If they are not, throws an error.
+        Returns true if all the arguments are valid.
+        '''
+
+        if args.request_bucket is None and args.request_queue is None:
+            raise ValueError()
+
+    def _save_base_arguments(self, args: object) -> None:
+        '''Saves the passed arguments into the class.'''
+        self.logger.debug('Saving WidgetAppBase arguments...')
+        self.profile:str = args.profile
+        self.region:str = args.region
+        self.max_runtime:int = args.max_runtime
+        self.request_bucket:str = args.request_bucket
+        self.use_owner_in_prefix: str = args.use_owner_in_prefix
+        self.request_queue_url:str = args.request_queue
+        self.logger.debug('WidgetAppBase arguments saved!')
+
+    def _get_basic_parser(self) -> ArgumentParser:
+        '''Returns a ArgumentParser filled with the basic
+        arguments needed for a Widget Application.
+        '''
+        parser = ArgumentParser()
+        
+        # Many of the help prompts are based off of the `producer/consumer.jar` examples
+        self.logger.debug('Adding basic WidgetApp arguments to parser...')
+        parser.add_argument('-p', '--profile',
+                            action='store',
+                            type=str,
+                            default='default',
+                            help='AWS profile to use for credentials (default: %(default)s)')
+        parser.add_argument('-r', '--region',
+                            action='store',
+                            type=str,
+                            default='us-east-1',
+                            help='Name of AWS region to use (default: %(default)s)')
+        parser.add_argument('-rb', '--request-bucket',
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='Name of bucket containing widget requests (default: %(default)s)')
+        parser.add_argument('-uop', '--use-owner-in-prefix',
+                            action='store_true',
+                            default=False,
+                            help="Use the owner's name in the S3 object's prefix " +
+                                "(default: %(default)s)")
+        parser.add_argument('-rq', '--request-queue',
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='URL of queue containing widget requests (default: %(default)s)')
+        self.logger.debug('WidgetApp argument options added! Returning parser.')
+
+        return parser
--- source/widget_consumer.py
+++ source/widget_consumer.py
@@ -1,27 +1,86 @@
+from argparse import ArgumentParser
 from widget_app_base import WidgetAppBase
 
 class WidgetConsumer(WidgetAppBase):
     def __init__(self) -> None:
         super().__init__()
+        self.logger.name = 'consumer_logger'
 
-    def get_requests_from_bucket():
+    def get_consumer_parser(self) -> ArgumentParser:
+        '''Returns the parser for the consumer'''
+        parser = self._get_basic_parser()
+        
+        # Many of the help prompts are based off of the `consumer.jar` example
+        self.logger.debug('Adding Consumer App arguments to parser...')
+        parser.add_argument('-mrt', '--max-runtime',
+                            action='store',
+                            type=int,
+                            default=0,
+                            help='Maximum runtime in milliseconds. 0 means no maximum ' + 
+                                '(default: %(default)s)')
+        parser.add_argument('-wb', '--widget-bucket',
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='Name of S3 bucket holding widgets (default: %(default)s)')
+        parser.add_argument('-wkp', '--widget-key-prefix',
+                            action='store',
+                            type=str,
+                            default='\'widgets/\'',
+                            help='Prefix for widget objects in S3 (default: %(default)s)')
+        parser.add_argument('-dwt', '--dynamodb-widget-table',
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='Name of DynamoDB table that holds widgets (default: %(default)s)')
+        parser.add_argument('-pdbc', '--pdb-conn',
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='Postgres Database connection string (default: %(default)s)')
+        parser.add_argument('-pdbu', '--pdb-username',
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='Postgres Database username (default: %(default)s)')
+        parser.add_argument('-pdbp', '--pdb-password',
+                            action='store',
+                            type=str,
+                            default=None,
+                            help='Postgres Database password (default: %(default)s)')
+        parser.add_argument('-qwt', '--queue-wait-time',
+                            action='store',
+                            type=int,
+                            default=10,
+                            help='The duration (in seconds) to wait for a message to arrive ' +
+                                'in the request when tring to receive a message ' +
+                                '(default: %(default)s)')
+        parser.add_argument('-qvt', '--queue-visibility-timeout',
+                            action='store',
+                            type=int,
+                            default=2,
+                            help='The duration (in seconds) to the messages received from the ' + 
+                                'queue are hidden from others (default: %(default)s)')
+        self.logger.debug('Consumer argument options added! Returning parser.')
+        
+        return parser
+
+    def get_requests_from_bucket(self):
         NotImplementedError()
         # time_running_ms:int = 0
         # DO
         #     TRY
         #         GET request from s3 bucket
         #         CONVERT request to json
+        #         delete_request(request)
         #
         #         IF request['type'] == 'create'
-        #             delete_request(request)
         #             create_widget(request)
         #             continue
         #         ELSE IF request['type'] == 'update'
-        #             delete_request(request)
         #             update_widget(request)
         #             continue
         #         ELSE IF request['delete'] == 'delete'
-        #             delete_request(request)
         #             delete_widget(request)
         #             continue
         #         ELSE
@@ -59,4 +118,9 @@ def verify_args():
     NotImplementedError()
 
 if __name__ == '__main__':
-    print('Hello World!')
+    app = WidgetConsumer()
+    parser = app.get_consumer_parser()
+    parser.print_help()
+
+    args = parser.parse_args()
+    print(args)
