8259112880113c0140fa8f675bc15e0e67a46df6
Journey Curtis <programming.jlcsly98@icloud.com>
2024-10-18 01:07:36 +0000

    add widget_app_base tests

--- test/test_widget_app_base.py
+++ test/test_widget_app_base.py
@@ -1,1 +1,41 @@
 '''Empty as there are no functions (yet) in widget_app_base.py'''
+from pytest import raises
+
+from source.widget_app_base import WidgetAppBase
+
+class ArgReplica:
+    def __init__(self) -> None:
+        self.profile:str = 'default'
+        self.region:str = 'us-east-1'
+        self.request_bucket:str = None
+        self.use_owner_in_prefix:bool = False
+        self.request_queue:str = None
+
+class TestWidgetAppBase:
+
+    def test_verify_base_arguments_request_bucket(self):
+        # setup
+        args = ArgReplica()
+        args.request_bucket = 'test'
+        app = WidgetAppBase()
+        
+        # exercise and verify
+        assert app._verify_base_arguments(args)
+
+    def test_verify_base_arguments_request_queue(self):
+        # setup
+        args = ArgReplica()
+        args.request_queue = 'test'
+        app = WidgetAppBase()
+        
+        # exercise and verify
+        assert app._verify_base_arguments(args)
+
+    def test_verify_base_arguments_no_request_bucket_no_request_queue(self):
+        # setup
+        args = ArgReplica()
+        app = WidgetAppBase()
+        
+        # exercise and verify
+        with raises(ValueError):
+            app._verify_base_arguments(args)
