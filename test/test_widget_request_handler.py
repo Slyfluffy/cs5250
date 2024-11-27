1a89c0cb0d6ba941bef768f087cb3c1301d0ee09
Journey Curtis <programming.jlcsly98@icloud.com>
2024-11-27 16:23:40 +0000

    stub widget_request_handler unit tests

---
+++ test/test_widget_request_handler.py
@@ -0,0 +1,20 @@
+
+
+class TestWidgetRequestHandler():
+    def test_valid_widget_request_create():
+        NotImplementedError()
+
+    def test_valid_widget_request_update():
+        NotImplementedError()
+    
+    def test_valid_widget_request_delete():
+        NotImplementedError()
+
+    def test_invalid_request_type():
+        NotImplementedError()
+
+    def test_widget_request_no_queue_url():
+        NotImplementedError()
+
+    def test_widget_request_client_error():
+        NotImplementedError()
