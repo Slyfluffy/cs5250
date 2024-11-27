8773c0a0591c8fb6ba4c49d5ec6c1e269e09d19b
Journey Curtis <programming.jlcsly98@icloud.com>
2024-11-27 17:43:01 +0000

    implement unit tests for widget_request_handler

--- test/test_widget_request_handler.py
+++ test/test_widget_request_handler.py
@@ -1,20 +1,136 @@
+from boto3 import client
+from json import dumps
+from moto import mock_aws
+from os import environ
 
+from source.widget_request_handler import handle_request
 
 class TestWidgetRequestHandler():
-    def test_valid_widget_request_create():
-        NotImplementedError()
+    @mock_aws
+    def test_valid_widget_request_create(self):
+        # setup
+        request = {
+            'requestId': '1',
+            'type': 'create',
+            'owner': 'tester'
+        }
 
-    def test_valid_widget_request_update():
-        NotImplementedError()
+        ## set environment variables
+        environ['REGION'] = 'us-east-1'
+        environ['QUEUE_URL'] = 'test.test'
+
+        ## setup client
+        sqs = client('sqs', region_name='us-east-1')
+
+        # Exercise
+        assert handle_request(request, sqs)
+
+        # verify
+        response:dict = sqs.receive_message(
+            QueueUrl=environ['QUEUE_URL'],
+            MaxNumberOfMessages=1
+        )
+        assert response['Messages'][0]["Body"]
+
+    def test_valid_widget_request_update(self):
+        # setup
+        request = {
+            'requestId': '1',
+            'type': 'create',
+            'owner': 'tester'
+        }
+
+        ## set environment variables
+        environ['REGION'] = 'us-east-1'
+        environ['QUEUE_URL'] = 'test.test'
+
+        ## setup client
+        sqs = client('sqs', region_name='us-east-1')
+
+        # Exercise
+        assert handle_request(request, sqs)
+
+        # verify
+        response:dict = sqs.receive_message(
+            QueueUrl=environ['QUEUE_URL'],
+            MaxNumberOfMessages=1
+        )
+        assert response['Messages'][0]["Body"]
     
-    def test_valid_widget_request_delete():
-        NotImplementedError()
+    def test_valid_widget_request_delete(self):
+        # setup
+        request = {
+            'requestId': '1',
+            'type': 'create',
+            'owner': 'tester'
+        }
 
-    def test_invalid_request_type():
-        NotImplementedError()
+        ## set environment variables
+        environ['REGION'] = 'us-east-1'
+        environ['QUEUE_URL'] = 'test.test'
 
-    def test_widget_request_no_queue_url():
-        NotImplementedError()
+        ## setup client
+        sqs = client('sqs', region_name='us-east-1')
 
-    def test_widget_request_client_error():
-        NotImplementedError()
+        # Exercise
+        assert handle_request(request, sqs)
+
+        # verify
+        response:dict = sqs.receive_message(
+            QueueUrl=environ['QUEUE_URL'],
+            MaxNumberOfMessages=1
+        )
+        assert response['Messages'][0]["Body"]
+
+    def test_invalid_request_type(self):
+        # setup
+        request = {
+            'requestId': '1',
+            'type': 'unknown',
+            'owner': 'tester'
+        }
+
+        ## set environment variables
+        environ['REGION'] = 'us-east-1'
+        environ['QUEUE_URL'] = 'test.test'
+
+        ## setup client
+        sqs = client('sqs', region_name='us-east-1')
+
+        # Exercise
+        assert not handle_request(request, sqs)
+
+    def test_widget_request_no_queue_url(self):
+        # setup
+        request = {
+            'requestId': '1',
+            'type': 'create',
+            'owner': 'tester'
+        }
+
+        ## set environment variables
+        environ['REGION'] = 'us-east-1'
+
+        ## setup client
+        sqs = client('sqs', region_name='us-east-1')
+
+        # Exercise
+        assert not handle_request(request, sqs)
+
+    def test_widget_request_client_error(self):
+        # setup
+        request = {
+            'requestId': '1',
+            'type': 'create',
+            'owner': 'tester'
+        }
+
+        ## set environment variables
+        environ['REGION'] = 'us-east-1'
+        environ['QUEUE_URL'] = 'test.test'
+
+        ## setup client
+        sqs = client('sqs', region_name='us-east-2')
+
+        # Exercise
+        assert not handle_request(request, sqs)
